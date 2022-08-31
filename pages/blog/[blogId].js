5c2917dc18806a3a69ab759b3ebb595339e3ae58
MortalMelvin <floresmm2001@gmail.com>
2022-08-31 12:58:01 +1000

    First Commit

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,42 @@
+import Head from 'next/dist/shared/lib/head'
+
+function Blog({ title, description }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+      </Head>
+      {/* <h1 className='content'>Env Analytics {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+      
+      </h1> */}
+      <h1 className='content'>
+        Env user {process.env.DB_USER} Password {process.env.DB_PASSWORD}
+      </h1>
+    </>
+  )
+}
+
+export default Blog
+
+export async function getStaticPaths() {
+  return {
+      paths: [{params: {blogId: '1'}}],
+      fallback: false,
+  }
+}
+
+export async function getStaticProps() {
+  const user = process.env.DB_USER
+  const password = process.env.DB_PASSWORD
+
+  console.log(
+    `Connecting to database with username ${user} and password ${password}`
+  )
+  return {
+    props: {
+      title: 'Article Title',
+      description: 'Article description',
+    },
+  }
+}
---
+++ pages/pets.js
@@ -0,0 +1,11 @@
+import img from '../public/1.jpg'
+
+function PetsPage() {
+    return (
+        <div>
+            Pet
+        </div>
+    )
+}
+
+export default PetsPage
